81c5172c879e04b697290dfef6a58aac0b5c3a8c
Schade9 <s.charlesderrick9@gmail.com>
2022-01-13 23:51:27 +0300

    Done

--- src/App.js
+++ src/App.js
@@ -1,43 +1,64 @@
 import './App.css';
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { db } from "./firebase_config";
 import firebase from "firebase";
+import TodoListItem from './Todo';
+
 
 function App() {
-
+  const [todos, setTodos] = useState([]);
   const [todoInput, setTodoInput] = useState('');
 
-  function addTodo() {
+  useEffect(() => {
+    getTodos();
+  }, []) // blank only to run on first launch
+
+  function getTodos() {
+    db.collection("todos").onSnapshot(function (querySnapshot) {
+      setrTodos(
+        querySnapshot.docs.map((doc) => ({
+          id: doc.id,
+          todo: doc.data().todo,
+          inprogress: doc.data().inprogress
+        }))
+      )
+    }) 
+  }
+
+  function addTodo(e) {
     e.preventDefault(); // prevent reload of page
 
+    // add todo to database
     db.collection("todos").add({
       inprogress: true,
       timestamp: firebase.firestore.FieldValue.serverTimestamp(),
       todo: todoInput,
     });
 
-    
+    setTodoInput("");
   }
 
   return (
-    <div className="App" 
-      style={{
-        display: "flex", 
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <div>
+    <div className="App" >
+      <div
+        style={{
+          display: "flex", 
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          width: "100%"
+        }}
+      >
         <h1>Schades Todos App</h1>
         <form>
           <TextField 
             id="standard-basic" 
             label="Write a Todo" 
             value={todoInput}
+            style={{width:"90vw", maxWidth: "500px"}}
             onChange={(e) => setTodoInput(e.target.value)}
-            style={{maxWidth: "300px", width: "90vw"}}
           />
           <Button 
             type="submit" 
@@ -48,6 +69,16 @@ function App() {
             Add
           </Button>
         </form>
+        
+        <div style={{width:"90vw", maxWidth: "500px", marginTop: "24px"}}>
+          {todos.map((todo) => (
+            <TodoListItem 
+              todo={todo.todo}
+              inprogress={todo.inprogress}
+              id={todo.id}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
--- src/firebase_config.js
+++ src/firebase_config.js
@@ -1,24 +1,17 @@
-// Import the functions you need from the SDKs you need
-import firebase from "firebase";
-//import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyATbbQc0fQ1IayF5cy1dgj972Ra9kH842o",
-  authDomain: "todo-list-294c4.firebaseapp.com",
-  projectId: "todo-list-294c4",
-  storageBucket: "todo-list-294c4.appspot.com",
-  messagingSenderId: "683542207996",
-  appId: "1:683542207996:web:e612381d2e46763c5aa679"
-};
+    apiKey: "AIzaSyATbbQc0fQ1IayF5cy1dgj972Ra9kH842o",
+    authDomain: "todo-list-294c4.firebaseapp.com",
+    projectId: "todo-list-294c4",
+    storageBucket: "todo-list-294c4.appspot.com",
+    messagingSenderId: "683542207996",
+    appId: "1:683542207996:web:e612381d2e46763c5aa679"
+  };
 
-// Initialize Firebase
-//const app = initializeApp(firebaseConfig);
+const firebaseApp = firebase.initializeApp(firebaseConfig);
 
-firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
 
-const db = firebase.firestore();
-
-export { db };
+export {db};
