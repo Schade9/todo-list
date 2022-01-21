a4453b82c116b496a896bc1e0fde850840c69782
Ssemakula Charles Derrick <51287876+Schade9@users.noreply.github.com>
2022-01-21 22:43:08 +0300

    Update firebase_config.js

--- src/firebase_config.js
+++ src/firebase_config.js
@@ -15,22 +15,3 @@ const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
 
 export default db;
-
-//import firebase from "firebase";
-
-// import { initializeApp } from "firebase/app";
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyATbbQc0fQ1IayF5cy1dgj972Ra9kH842o",
-//   authDomain: "todo-list-294c4.firebaseapp.com",
-//   projectId: "todo-list-294c4",
-//   storageBucket: "todo-list-294c4.appspot.com",
-//   messagingSenderId: "683542207996",
-//   appId: "1:683542207996:web:e612381d2e46763c5aa679"
-// };
-
-// const app = initializeApp(firebaseConfig);
-
-// const db = app.firestore();
-
-// export default db;
